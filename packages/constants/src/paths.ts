1aa16adc6e43b661f7cda79debc8b5bdf1f53677
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-09-18 07:16:28 -0700

    chore: add more paths constants

--- packages/constants/src/paths.ts
+++ packages/constants/src/paths.ts
@@ -1,11 +1,12 @@
-import { homedir } from "node:os";
-import { join } from "node:path";
+import { join } from "node:path"
+import { getMusicDir } from "./helpers"
 
-export const HOME_DIR = join(homedir(), ".ongaku");
+export const HOME_DIR = join(getMusicDir(), "ongaku")
 
-export const PLAYLIST_DIR = join(HOME_DIR, "playlists");
+export const PLAYLIST_DIR = join(HOME_DIR, "playlists")
+export const PLAYLIST_DEFAULT_DIR = join(PLAYLIST_DIR, "Default")
 
-export const CACHE_DIR = join(HOME_DIR, "cache");
-export const CACHE_PICTURES_DIR = join(CACHE_DIR, "pictures");
+export const CACHE_DIR = join(HOME_DIR, "cache")
+export const CACHE_PICTURES_DIR = join(CACHE_DIR, "pictures")
 
-export const BIN_DIR = join(HOME_DIR, "bin");
+export const BIN_DIR = join(HOME_DIR, "bin")
