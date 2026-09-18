aa656335d7738c4f61b333b164d87f458fc20577
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-09-18 07:18:05 -0700

    chore: minor changes

--- apps/desktop/src/renderer/src/routes/__root.tsx
+++ apps/desktop/src/renderer/src/routes/__root.tsx
@@ -1,5 +1,7 @@
+import { Player } from "@renderer/components/blocks/player";
 import { TanstackDevtoolsComponent } from "@renderer/components/blocks/tanstack-devtools";
 import { ThemeProvider } from "@renderer/components/blocks/theme-provider";
+import { Toaster } from "@renderer/components/ui/sonner";
 import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 
@@ -16,6 +18,9 @@ function RootLayout() {
     <ThemeProvider defaultTheme="system" storageKey="ui-theme">
       <main className="flex flex-col justify-between w-full h-screen">
         <Outlet />
+        <Player />
+
+        <Toaster />
         <TanstackDevtoolsComponent />
       </main>
     </ThemeProvider>
--- apps/desktop/src/renderer/src/routes/index.tsx
+++ apps/desktop/src/renderer/src/routes/index.tsx
@@ -1,7 +1,10 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
+  beforeLoad: () => {
+    throw redirect({ to: "/playlists" });
+  },
 });
 
 function RouteComponent() {
