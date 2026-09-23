b5587c7456ed71f4abcddc7f69dd9079fc143ee6
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-09-23 04:29:20 -0500

    chore: add song name tooltiped

--- src/components/player/player-cover.tsx
+++ src/components/player/player-cover.tsx
@@ -1,21 +1,34 @@
 import { CoverImage } from "@/components/cover-image"
 import { getCoverUrl } from "@/lib/song-utils"
 import { usePlayerStore } from "@/shared/stores/use-player"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger
+} from "@/components/ui/tooltip"
 
 export function PlayerCover() {
   const currentSong = usePlayerStore((state) => state.currentSong)
-
   if (!currentSong) return null
 
   const coverUrl = getCoverUrl({ song: currentSong })
 
   return (
-    <figure className="w-20 h-20 shrink">
-      <CoverImage
-        key={currentSong.id}
-        src={coverUrl}
-        alt={currentSong.name}
+    <Tooltip>
+      <TooltipTrigger
+        render={
+          <figure className="w-20 h-20 shrink">
+            <CoverImage
+              key={currentSong.id}
+              src={coverUrl}
+              alt={currentSong.name}
+            />
+          </figure>
+        }
       />
-    </figure>
+      <TooltipContent>
+        <p>{currentSong.name}</p>
+      </TooltipContent>
+    </Tooltip>
   )
 }
