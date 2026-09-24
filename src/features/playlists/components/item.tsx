4fc1cd78c2a73b2c13c74c1a8ed7d5cdf5689bb3
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-09-24 18:18:21 -0500

    chore: highlight link if active current playlist

--- src/features/playlists/components/item.tsx
+++ src/features/playlists/components/item.tsx
@@ -1,23 +1,31 @@
 import type { TPlaylist } from "@/shared/types/playlist.types"
-import { Link } from "@tanstack/react-router"
-import { PlaylistItemActions } from "./actions"
+import { Link, useLocation } from "@tanstack/react-router"
+import { PlaylistItemActions } from "@/features/playlists/components"
 
 interface PlaylistItemProps {
   playlist: TPlaylist
 }
 
 export function PlaylistItem({ playlist }: PlaylistItemProps) {
+  const pathname = useLocation({
+    select: (location) => location.pathname
+  })
+  const isSamePath = pathname
+    .toLowerCase()
+    .includes(playlist.name.toLowerCase())
+
   return (
     <div className="w-full relative group/item">
       <Link
         to="/playlists/$playlistName"
         params={{ playlistName: playlist.name }}
-        activeOptions={{ exact: true }}
-        activeProps={{ className: "font-bold" }}
       >
-        <div className="w-full hover:bg-accent p-2 flex items-center justify-between">
-          <h2>{playlist.name}</h2>
-        </div>
+        <h3
+          data-same-path={isSamePath}
+          className="p-2 w-full data-[same-path=true]:bg-accent"
+        >
+          {playlist.name}
+        </h3>
       </Link>
 
       <div className="flex absolute right-2 inset-y-0 h-full items-center">
