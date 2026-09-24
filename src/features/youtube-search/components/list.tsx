c5f34f27ec7d8c43ea9515b1745b3cb03a98d384
miguedev1047 <miguelangeljimenez1047@gmail.com>
2026-09-24 16:57:07 -0500

    chore: update the empty statet component

--- src/features/youtube-search/components/list.tsx
+++ src/features/youtube-search/components/list.tsx
@@ -2,13 +2,35 @@ import { youtubeSearchQueryOpts } from "@/shared/queries/youtube"
 import { useSuspenseQuery } from "@tanstack/react-query"
 import { useSearch } from "@tanstack/react-router"
 import { SearchYoutubeItem } from "@/features/youtube-search/components"
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle
+} from "@/components/ui/empty"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { ListIcon } from "@hugeicons/core-free-icons"
 
 export function SearchYoutubeList() {
   const { q } = useSearch({ from: "/search-youtube/" })
+
   const { data: results = [] } = useSuspenseQuery(youtubeSearchQueryOpts(q))
 
-  if (results.length === 0) {
-    return <p>No results available</p>
+  if (!results.length) {
+    return (
+      <div className="h-full flex flex-col items-center justify-center text-center p-8 gap-3 text-muted-foreground">
+        <Empty className="py-16">
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <HugeiconsIcon icon={ListIcon} />
+            </EmptyMedia>
+            <EmptyTitle>No results</EmptyTitle>
+            <EmptyDescription>There are no songs results</EmptyDescription>
+          </EmptyHeader>
+        </Empty>
+      </div>
+    )
   }
 
   const renderItems = results.map((item) => (
